18a411bf8fb62298dba2ca3b9765c0d0ed636789
garg_pulkit <gargpulkit007@yahoo.com>
2018-09-09 00:58:42 -0700

    Adding first check-in

---
+++ UseOfGenerics/src/GenericClassExample.java
@@ -0,0 +1,42 @@
+
+class testGeneric<T extends Number>
+{
+	T obj;
+	
+	testGeneric(T o)
+	{
+		this.obj=o;
+	}
+	
+	T getValue()
+	{
+		return obj;
+	}
+	
+	void showType()
+	{
+		System.out.println(obj.getClass().getName());
+	}
+	
+	
+		
+}
+
+public class GenericClassExample {
+	
+	public static void main(String[] args)
+	{
+		testGeneric<Integer> t = new testGeneric(66);
+		
+		t.getClass();
+		System.out.println(t.getValue());
+		
+		
+		/*testGeneric<String> str = new testGeneric("Pulkit Garg");
+		str.getClass();
+		System.out.println(str.getValue());*/
+	}
+	
+	
+
+}
